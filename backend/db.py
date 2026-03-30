222bd37399a4ff0de241c502bf87a17740497f55
Michael Bino <mabino@gmail.com>
2026-03-29 20:39:06 -0400

    Update models with RecentContact and GoogleContact tables

--- backend/db.py
+++ backend/db.py
@@ -6,7 +6,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./prosciutto.db")
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 def create_db_and_tables():
-    from backend.models import Account, Setting # Ensure they are imported before calling create_all
+    from backend.models import Account, Setting, RecentContact, GoogleContact # Ensure they are imported before calling create_all
     SQLModel.metadata.create_all(engine)
 
 def get_session():
