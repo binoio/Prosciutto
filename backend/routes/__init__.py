709782de9e712a07cb7b608c2cac4cf7a3f1fd9b
Michael Bino <mabino@gmail.com>
2026-03-31 21:04:38 -0400

    refactor: update main.py to use APIRouter and Services

---
+++ backend/routes/__init__.py
@@ -0,0 +1,5 @@
+from .auth import router as auth
+from .accounts import router as accounts
+from .messages import router as messages
+from .contacts import router as contacts
+from .settings import router as settings
